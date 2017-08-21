5d9eb735a7a87b775d813bd1ee1e7c6806714944
anatolii <anatolii@example.com>
2017-08-21 18:06:12 +0300

    Add user menu methods

--- MyBank/Program.cs
+++ MyBank/Program.cs
@@ -24,7 +24,7 @@ namespace MyBank
                 switch (action)
                 {
                     case 1:
-                        Authorization(authorization);
+                        Authorization(ref authorization);
                         break;
                     case 2:
                         return;
@@ -34,7 +34,7 @@ namespace MyBank
                 }
             } while (true);
         }
-        static void Authorization(string[,] authorization)
+        static void Authorization(ref string[,] authorization)
         {
             string login = null, password = null;
 
@@ -50,16 +50,19 @@ namespace MyBank
                     if (login == authorization[i, 0])
                     {
                         isLogin = true;
-                        if (password == authorization[i, 1] && login == "Admin")
+                        if (login == "Admin" && password == authorization[i, 1])
                         {
-                            if (AdminMenu(authorization))
+                            if (AdminMenu(ref authorization))
                             {
                                 return;
                             }
                         }
-                        else if (password == authorization[i, 1] && login == "User")
+                        else if (password == authorization[i, 1])
                         {
-                            Console.WriteLine("Меню пользователя.");
+                            if(UserMenu(authorization, login))
+                            {
+                                return;
+                            }
                         }
                         else
                         {
@@ -79,7 +82,7 @@ namespace MyBank
                 }
             } while (true);
         }
-        static bool AdminMenu(string[,] authorization)
+        static bool AdminMenu(ref string[,] authorization)
         {
             string action = null;
             do
@@ -111,6 +114,21 @@ namespace MyBank
                 }
             } while (true);
         }
+        static void ShowListAccount(string[,] authorization)
+        {
+            if (authorization.GetLength(0) > 1)
+            {
+                Console.WriteLine("Имя | " + "Счет | " + "Статус");
+                for (int i = 1; i < authorization.GetLength(0); i++)
+                {
+                    Console.WriteLine("{0} | {1} | {2}", authorization[i, 0], authorization[i, 2], authorization[i, 3]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Список клиентов пуст.");
+            }
+        }
         static void BankAccountBlocked(ref string[,] authorization)
         {
             int index;
@@ -133,7 +151,6 @@ namespace MyBank
                 }
             } while (true);
         }
-
         static void BankAccountUnblocked(ref string[,] authorization)
         {
             int index;
@@ -193,6 +210,7 @@ namespace MyBank
                         }
                     }
                     newAuthorization[newAuthorization.GetLength(0) - 1, 0] = userName;
+                    newAuthorization[newAuthorization.GetLength(0) - 1, 1] = userName;
                     newAuthorization[newAuthorization.GetLength(0) - 1, 2] = userAccount;
                     newAuthorization[newAuthorization.GetLength(0) - 1, 3] = "Unblocked";
                     return newAuthorization;
@@ -246,21 +264,7 @@ namespace MyBank
                 }
             } while (true);
         }
-        static void ShowListAccount(string[,] authorization)
-        {
-            if (authorization.GetLength(0) > 1)
-            {
-                Console.WriteLine("Имя | " + "Счет | " + "Статус");
-                for (int i = 1; i < authorization.GetLength(0); i++)
-                {
-                    Console.WriteLine("{0} | {1} | {2}", authorization[i, 0], authorization[i, 2], authorization[i, 3]);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Список клиентов пуст.");
-            }
-        }
+        
         static int ExistName(string[,] authorization, string name)
         {
             for (int i = 1; i < authorization.GetLength(0); i++)
@@ -271,6 +275,102 @@ namespace MyBank
                 }
             }
             return -1;
+        }
+
+        static bool UserMenu(string[,] authorization, string userName)
+        {
+            string action = null;
+            do
+            {
+                Console.WriteLine("Выберите действие: \n 1-Просмотреть счет \n 2-Пополнить счет \n 3-Снять деньги со счета \n 4-Вернуться в главное меню");
+                action = Console.ReadLine();
+                switch (action)
+                {
+                    case "1":
+                        Console.WriteLine("Метод просмотра счета");
+                        ShowBalance(authorization, userName);
+                        break;
+                    case "2":
+                        Console.WriteLine("Метод пополнения счета");
+                        AccountReplenish(ref authorization, userName);
+                        break;
+                    case "3":
+                        Console.WriteLine("Метод снятия денег со счета");
+                        CashWithdrawal(ref authorization, userName);
+                        break;
+                    case "4":
+                        return true;
+                    default:
+                        Console.WriteLine("Неизвестная операция");
+                        break;
+                }
+            } while (true);
+        }
+        static void ShowBalance(string[,] authorization, string userName)
+        {
+            for (int i = 0; i < authorization.GetLength(0); i++)
+            {
+                if(authorization[i, 0] == userName)
+                {
+                    Console.WriteLine("Ваш баланс = {0}.", authorization[i, 2]);
+                }
+            }
+        }
+        static void AccountReplenish(ref string[,] authorization, string userName)
+        {
+            int money = 0;
+            for (int i = 0; i < authorization.GetLength(0); i++)
+            {
+                if(authorization[i, 0] == userName)
+                {
+                    do
+                    {
+                        Console.WriteLine("Введите сумму для пополнения:");
+                        money = int.Parse(Console.ReadLine());
+                        if (money > 0)
+                        {
+                            authorization[i, 2] = (int.Parse(authorization[i, 2]) + money).ToString();
+                            return;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Не корректная сумма! Попробовать еще раз? (Enter/нет)");
+                            if (Console.ReadLine() == "нет")
+                            {
+                                return;
+                            }
+                        }
+                    } while (true);
+                }
+            }
+        }
+        static void CashWithdrawal(ref string[,] authorization, string userName)
+        {
+            int money = 0;
+            for (int i = 0; i < authorization.GetLength(0); i++)
+            {
+                if (authorization[i, 0] == userName)
+                {
+                    do
+                    {
+                        Console.WriteLine("Введите сумму для снятия:");
+                        money = int.Parse(Console.ReadLine());
+                        if (money > 0 && money <= int.Parse(authorization[i, 2]))
+                        {
+                            authorization[i, 2] = (int.Parse(authorization[i, 2]) - money).ToString();
+                            return;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Не корректная сумма! На вашем счету {0}. Попробовать еще раз? (Enter/нет)", authorization[i, 2]);
+                            if (Console.ReadLine() == "нет")
+                            {
+                                return;
+                            }
+                        }
+                    } while (true);
+                }
+            }
         }
     }
 }
